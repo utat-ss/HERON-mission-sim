7c77d9b10ab248e4faa8dc0ce105d482dc299022
Kim <kim.shek@mail.utoronto.ca>
2019-04-12 16:26:01 -0400

    Wrote first version of power sim code that hasn't been tested yet

--- notebooks/Power_Sims.py
+++ notebooks/Power_Sims.py
@@ -63,12 +63,16 @@ n_orbits = 3
 n_points = n_orbits * n_pts_per_orbit
 t_sim = n_points * dt
 
+powerIn = []
+powerOut = []
 for i in range(n_points):
     heron.set_state(i)
     heron.draw_powers()
     heron.update_thermal(total_areas[i % len(areas)], areas['negZ'][i%len(areas)], heron.batt_current_net)
     heron.charge_from_solar_panel(panel_areas[i % len(areas)] / (0.03 * 0.01), dt)
     heron.update_state_tracker(i*dt)
+    powerIn.append(heron.power_in)
+    powerOut.append(heron.power_out)
 
 #for actual DC supply / Eload code.
 
@@ -81,29 +85,38 @@ print (DC.query("*IDN?"))
 #start scripting:
 power = eload.query_ascii_values("FETC:POW? ")
 
+for i in range(len(powerIn)):
+    powerIn_watts = eload.write('POW', str(powerIn[i]))
+for i in range(len(powerOut)):
+    powerOut_watts = DC.write('POW', str( powerOut[i]))
 
-areas_in = []
-with open(r'D:\Ali\Documents\UTAT\1000hr\heron_area_frac.csv','r') as csvFile:
-    reader = csv.reader(csvFile)
-    for row in reader:
-        areas_in.append(n.float32(row[0]))
 
-eload.write('CURR 0.01')
-for i in range(len(areas_in)):
-    current_ma = 500 * 3 * 1 * areas_in[i] / 1000.0
 
-    dc_output = DC.write("APPL CH3, 5, ", str(current_ma))
-    if i % 15 == 0:
-        print ("Minute: ", i / 60.0)
-        print ("Current (A): ", current_ma)
-        print(dc_output)
 
-    if i%60 == 1:
-        print('Comms ON')
-        eload.write('CURR 0.6')
 
-    if i%60 == 20:
-        print ("Comms OFF")
-        eload.write('CURR 0.01')
-
-    time.sleep(1)
+#Step 1: obtaining power from
+# areas_in = []
+# with open(r'D:\Ali\Documents\UTAT\1000hr\heron_area_frac.csv','r') as csvFile:
+#     reader = csv.reader(csvFile)
+#     for row in reader:
+#         areas_in.append(n.float32(row[0]))
+#
+# eload.write('CURR 0.01')
+# for i in range(len(areas_in)):
+#     current_ma = 500 * 3 * 1 * areas_in[i] / 1000.0
+#
+#     dc_output = DC.write("APPL CH3, 5, ", str(current_ma))
+#     if i % 15 == 0:
+#         print ("Minute: ", i / 60.0)
+#         print ("Current (A): ", current_ma)
+#         print(dc_output)
+#
+#     if i%60 == 1:
+#         print('Comms ON')
+#         eload.write('CURR 0.6')
+#
+#     if i%60 == 20:
+#         print ("Comms OFF")
+#         eload.write('CURR 0.01')
+#
+#     time.sleep(1)
